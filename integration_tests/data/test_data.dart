301a7f75c8b1320072f46355163b66134971758b
Aman Kumar <aman@Amans-MacBook-Pro.local>
2024-07-23 13:35:36 +0200

    added third test

--- integration_tests/data/test_data.dart
+++ integration_tests/data/test_data.dart
@@ -2,6 +2,7 @@ class TestData {
   static const String searchQuery = "JUSTIN BIEBER";
   static const String artistName = "Justin Bieber";
   static const String albumName = "Believe";
+  static const String albumNameError = "Justice";
   static const String trackName = '1-All Around the World';
   static const String tracksTitle = 'Believe Tracks:';
 }
---
+++ integration_tests/tests/error_handling_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:music_app/main.dart' as app;
+import '../pages/home_page.dart';
+import '../pages/search_page.dart';
+import '../pages/album_page.dart';
+import '../data/test_data.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('Verify error message when failing to add album to favorites', (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      final homePage = HomePage(tester);
+      final searchPage = SearchPage(tester);
+      final albumPage = AlbumPage(tester);
+
+      // Search for artist
+      await homePage.openSearch();
+      await searchPage.enterSearchQuery(TestData.searchQuery);
+
+      // Select artist
+      await searchPage.selectArtist(TestData.artistName);
+
+      // Select album to addin Favorite
+      await albumPage.addAlbumToFavorites(TestData.albumNameError);
+      await tester.pump(Duration(seconds: 3));
+
+       // Verify error message is displayed
+      final findsOneWidget = find.text('Sorry! we are unable to save your album ${TestData.albumNameError}');
+      expect(findsOneWidget, findsOneWidget);
+
+    });
+  });
+}
